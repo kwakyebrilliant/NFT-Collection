c556f61502501d57fe8a227e28332d57bb9cd3c2
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-03-19 18:56:15 +0000

    final commit

---
+++ constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xB78ca7A945E9AB5673cd549A48161059aA384465";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "http://localhost:3000/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
